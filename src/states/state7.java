fb2bfdcd2ef2671a6f6d4fd5f12a74f540b9b000
SicaD <gabrielhdwerneck@gmail.com>
2018-03-24 14:52:16 -0300

    New States

---
+++ src/states/state7.java
@@ -0,0 +1,19 @@
+
+package states;
+
+public class state7 {
+    
+    public void takeAChar (char c)
+        {
+            if (c == '*')
+            {
+               // state 8
+            }
+            else
+            {
+               // nada 
+            }
+
+        }
+    
+}
---
+++ src/states/state8.java
@@ -0,0 +1,20 @@
+
+package states;
+
+
+public class state8 {
+    
+    public void takeAChar (char c)
+        {
+            if (c == '/')
+            {
+               // return new Token(Type.SMB_CSC, "*/", row, column); 
+            }
+            else
+            {
+                // retorna pra state7
+            }
+
+        }
+    
+}
