53d54d1a8992741011bfd43388647e8d612123b0
SicaD <gabrielhdwerneck@gmail.com>
2018-03-24 14:33:50 -0300

    States 1 & 2

--- src/states/state1.java
+++ src/states/state1.java
@@ -13,8 +13,126 @@ public class state1
 
     public void takeAChar (char c)
    {
-   
+       Token Token;
+       
+    
+	switch(c) 
+	{
+		case '$':
+                {
+                    //end of file
+                }
+		break;
+            
+                case ' ':
+                {
+                    // cont de coluna
+		}
+                break;
+                
+                case '\t':
+                {
+                    // 3 cont coluna
+                }
+                break;
+                
+                case '\n':
+                {
+                    // cont linha
+                }
+                break;
+                
+                case '\r':
+                {
+                    // sei la
+                }
+                break;
 
-   }
+		case '=': // state 2
+		{
+                    System.out.println("meee");
+		}
+                break;
+
+		case '!': // state 5
+		{
+                    
+		}
+                break;
+
+		case '<': // state 7
+		{
+                 //   return new Token(Type.OP_LE, "<", row, column);
+		}
+                break;
+            
+		case '>': // state 10
+		{
+                  //  return new Token(Type.OP_GT, ">", row, column);
+		}
+                break;
+
+		case '*': // state 17
+		{
+                  //  return new Token(Type.OP_MULT, "*", row, column);
+		}
+                break;
+
+		case '+': // state 18
+		{
+                 //   return new Token(Type.OP_AD, "+", row, column);
+		}
+                break;
+
+		case '-': // state 19
+                {
+                  //  return new Token(Type.OP_MIN, "-", row, column);
+		}
+                break;
+
+		case '/': // state 23
+		{
+		
+		}
+		break;
+            
+                case ',': // state 31
+		{
+                   // return new Token(Type.SMB_COM, ",", row, column);
+		}
+		break;
+            
+                case ';': // state 30
+		{
+                  //  return new Token(Type.SMB_SEM, ";", row, column);
+		}
+		break;
+            
+                case '(': // state 22
+		{
+                  //  return new Token(Type.SMB_OPA, "(", row, column);
+		}
+		break;
+            
+                case ')': // state 29
+		{
+                  //  return new Token(Type.SMB_CPA, ")", row, column);
+		}
+		break;
+            
+                case '{': // state 20
+		{
+                  //  return new Token(Type.SMB_OBC, "{", row, column);
+		}
+		break;
+            
+                case '}': // state 21
+		{
+                 //   return new Token(Type.SMB_CBC, "}", row, column);
+		}
+		break;
+	}
+    }
+       
    
 }
--- src/states/state2.java
+++ src/states/state2.java
@@ -1,10 +1,23 @@
 
 package states;
 
+import Util.Type;
+
 public class state2
     {
         public void takeAChar(char c)
-            {
+           {
+               
+    {
+        if (c == '=')
+        {
+           // return new Token(Type.OP_EQ, "==", )
+        }
+        else
+        {
+            
+        }    
+    }
                 
             }
     }
