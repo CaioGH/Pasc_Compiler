c477e9c3a66a79450c3a31dd16a0e7614445fa01
SicaD <gabrielhdwerneck@gmail.com>
2018-04-09 00:22:22 -0300

    Tokens para a Tabela de Simbolo

--- src/test/Compiler.java
+++ src/test/Compiler.java
@@ -10,27 +10,26 @@ public static void main(String[] args) throws IOException
         {
       
         Lexer lexer = new Lexer ("teste1_correto.jvn");
-        // parametro do Lexer: Um programa de acordo com a gramatica
-
 
         Token token;
         TS = new TabelaSim ();
 
-        // Enquanto não houver erros ou não for fim de arquivo:
+
         do
           {
             token = lexer.proxToken ();
 
-            // Imprime token
+ 
             if (token != null)
               {
 
                 System.out.println ("Token: " + token.toString () + "\t Linha: " + line + "\t Coluna: " + column);
+                TS.put(token, new Identifier());
 
-                // Verificar se existe o lexema na tabela de símbolos
               }
 
           }
+        
         while (token != null && token.getClassType () != Type.EOF);
         lexer.closeFile ();
 }
