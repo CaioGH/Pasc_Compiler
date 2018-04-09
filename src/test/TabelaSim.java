3d738f1c83a5ebc7e73b3d5ae68621ce64bb0f5b
SicaD <gabrielhdwerneck@gmail.com>
2018-04-09 00:27:03 -0300

    Remoção dos comentários

--- src/test/TabelaSim.java
+++ src/test/TabelaSim.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package test;
 
 import java.util.HashMap;
 
-/**
- *
- * @author caios
- */
 public class TabelaSim {
 
-    private HashMap<Token, Identifier> symbolTable; // Tabela de símbolos do ambiente
+    private HashMap<Token, Identifier> symbolTable; 
 
     public TabelaSim() {
         symbolTable = new HashMap();
 
-        // Inserindo as palavras reservadas
         Token word;
         word = new Token(Type.KW, "program", 0, 0);
         this.symbolTable.put(word, new Identifier());
@@ -58,14 +49,11 @@ public class TabelaSim {
         symbolTable.put(w, i);
     }
 
-    // Retorna um identificador de um determinado token
     public Identifier getIdentifier(Token w) {
         Identifier infoIdentificador = (Identifier) symbolTable.get(w);
         return infoIdentificador;
     }
 
-    // Pesquisa na tabela de símbolos se há algum tokem com determinado lexema
-    // vamos usar esse metodo somente para diferenciar ID e KW
     public Token returnToken(String lexeme) {
         for (Token token : symbolTable.keySet()) {
             if (token.getLexeme().equals(lexeme)) {
