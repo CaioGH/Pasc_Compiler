f8715080844a9df2746935107a7d6e92967ad8f6
SicaD <gabrielhdwerneck@gmail.com>
2018-05-27 20:09:39 -0300

    Mudança no main para a leitura do parser.

--- src/Compiler/Compiler.java
+++ src/Compiler/Compiler.java
@@ -10,28 +10,14 @@ public static void main(String[] args) throws IOException
         {
       
         Lexer lexer = new Lexer ("teste1_correto.jvn");
-
+       
         Token token;
         TS = new TabelaSim ();
-
-
-        do
-          {
-            token = lexer.proxToken ();
-
- 
-            if (token != null)
-              {
-
-                System.out.println ("Token: " + token.toString () + "\t Linha: " + line + "\t Coluna: " + column);
-                TS.put(token, new Identifier());
-
-              }
-
-          }
         
-        while (token != null && token.getClassType () != Type.EOF);
-        lexer.closeFile ();
+        Parser parser = new Parser(lexer);
+        parser.Prog();
+        
+        TS.show();
 }
 
 }
